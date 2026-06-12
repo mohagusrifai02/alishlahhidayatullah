223521ca27e6618fcede5991b9e8988ed2621110
agus rifai <agus0815@gmail.com>
2026-06-12 18:39:09 +0700

    sandbox

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -12,7 +12,7 @@ export async function POST(request: Request) {
     }
 
     const snap = new (midtransClient as any).Snap({
-      isProduction: true, 
+      isProduction: false, 
       serverKey: process.env.MIDTRANS_SERVER_KEY,
       clientKey: process.env.MIDTRANS_CLIENT_KEY,
     });
